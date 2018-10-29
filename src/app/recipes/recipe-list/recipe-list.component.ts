d3d8ff65fdd751086b894f4f7db0c220d08e338c
Shawn Jackson <shawn.anthony.j.95@gmail.com>
2018-10-29 00:43:00 -0400

    more component functionality for recipe and shopping lists

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from '../recipe.model';
 
 @Component({
@@ -11,14 +11,19 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('food', 'something tasty', 'https://i.ndtvimg.com/i/2017-10/diwali-food-menu_620x350_71507898681.jpg?down' +
       'size=630:380&output-quality=70&output-format=webp'),
-    new Recipe('food', 'something tasty', 'https://i.ndtvimg.com/i/2017-10/diwali-food-menu_620x350_71507898681.jpg?down' +
+    new Recipe('more food', 'another tasty thing', 'https://i.ndtvimg.com/i/2017-10/diwali-food-menu_620x350_71507898681.jpg?down' +
       'size=630:380&output-quality=70&output-format=webp')
   ];
-
+  @Output() recipeSelected = new EventEmitter<Recipe>();
   constructor() {
   }
 
   ngOnInit() {
+    //this.onRecipeSelect(this.recipes[0]);
+  }
+
+  onRecipeSelect(recipe: Recipe) {
+    this.recipeSelected.emit(recipe);
   }
 
 }
